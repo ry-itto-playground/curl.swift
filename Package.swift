11e34716c69cfaea3f1e884d78bb7f080e928505
ryoya ito <horusu.dqx@gmail.com>
2020-03-16 01:03:39 +0900

    wip

--- Package.swift
+++ Package.swift
@@ -8,13 +8,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "curl.swift",
-            dependencies: []),
+            dependencies: [
+                "ArgumentParser"
+            ]),
         .testTarget(
             name: "curl.swiftTests",
             dependencies: ["curl.swift"]),
--- Sources/curl.swift/main.swift
+++ Sources/curl.swift/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct Curl: ParsableCommand {
+
+    static var configuration: CommandConfiguration = .init(
+        commandName: "curl-swift",
+        abstract: "transfer a URL",
+        discussion: "")
+
+    @Argument(help: "URL")
+    var url: String
+
+    func run() throws {
+        print(url)
+    }
+}
+
+Curl.main()
